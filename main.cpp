a4dc4dc8240f21aacb895b4194221132bac33b97
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-11-12 15:04:56 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+/*
+Instructions
+Write a program to test the function you designed in Exercise 28 of this chapter.
+
+Use the following list to test your program.
+
+125, 100, 94, 88, 75, 71, 66, 58,
+53, 37, 32, 28, 14, 10, 8
+ */
